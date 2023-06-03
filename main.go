45b6874dd3a063bfe085cf19172193d3e7741254
abdullah-alaadine <abdullahalaadine63@gmail.com>
2023-06-03 21:08:42 +0300

    create scanner, scan Stdin

--- main.go
+++ main.go
@@ -1,5 +1,17 @@
 package main
 
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
 func main() {
-	
+	scanner := bufio.NewScanner(os.Stdin)
+
+	fmt.Println(" > ")
+	scanner.Scan()
+	text := scanner.Text()
+
+	fmt.Println("echoing: ", text)
 }
