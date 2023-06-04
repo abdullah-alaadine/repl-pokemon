89ee4fccfe86567b81d9dc6a08b6e2757d9c807c
abdullah-alaadine <abdullahalaadine63@gmail.com>
2023-06-04 16:31:55 +0300

    implement 'NewClient' function

--- internal/pokeapi/pokeapi.go
+++ internal/pokeapi/pokeapi.go
@@ -1,5 +1,22 @@
 package pokeapi
 
+import (
+	"net/http"
+	"time"
+)
+
+type Client struct {
+	httpClient http.Client
+}
+
+func NewClient() Client {
+	return Client{
+		httpClient: http.Client{
+			Timeout: time.Minute,
+		},
+	}
+}
+
 type LocationAreasResponse struct {
 	Count    int     `json:"count"`
 	Next     *string `json:"next"`
