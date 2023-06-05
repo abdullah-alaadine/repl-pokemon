16b70d5515d9a0ab9a0ceca598e947bcfc57dbb6
abdullah-alaadine <abdullahalaadine63@gmail.com>
2023-06-06 02:32:21 +0300

    return errors instead of 'log.Fatal(err)'

--- command_map.go
+++ command_map.go
@@ -3,13 +3,12 @@ package main
 import (
 	"errors"
 	"fmt"
-	"log"
 )
 
 func callbackMap(cfg *config) error {
 	locationAreas, err := cfg.pokeapiClient.ListLocationAreas(cfg.nextLocationAreaURL)
 	if err != nil {
-		log.Fatal(err)
+		return err
 	}
 	fmt.Println("Location Areas:")
 	for _, area := range locationAreas.Results {
@@ -26,7 +25,7 @@ func callbackMapb(cfg *config) error {
 	}
 	locationAreas, err := cfg.pokeapiClient.ListLocationAreas(cfg.prevLocationAreaURL)
 	if err != nil {
-		log.Fatal(err)
+		return err
 	}
 	fmt.Println("Location Areas:")
 	for _, area := range locationAreas.Results {
