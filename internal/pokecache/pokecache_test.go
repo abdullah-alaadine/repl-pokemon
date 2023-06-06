1e15c7a798bb5ed61a24d9a8a7491b8e0f963273
abdullah-alaadine <abdullahalaadine63@gmail.com>
2023-06-06 17:36:45 +0300

    write test for adding and getting cache

--- internal/pokecache/pokecache_test.go
+++ internal/pokecache/pokecache_test.go
@@ -8,3 +8,16 @@ func TestCreateTest(t *testing.T) {
 		t.Errorf("cache is nil")
 	}
 }
+
+func TestAddGetCache(t *testing.T) {
+	cache := NewCache()
+
+	cache.Add("key1", []byte("val1"))
+	actual, ok := cache.Get("key1")
+	if !ok {
+		t.Errorf("key1 not found")
+	}
+	if string(actual) != "val1" {
+		t.Errorf("value doesn't match")
+	}
+}
