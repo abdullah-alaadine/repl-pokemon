432250a6c9fe58dbaa415ce86c6a45245adf4e00
abdullah-alaadine <abdullahalaadine63@gmail.com>
2023-06-06 18:17:24 +0300

    implement 'reapLoop' method to delete cached data after a specific interval of time

--- internal/pokecache/pokecache.go
+++ internal/pokecache/pokecache.go
@@ -11,10 +11,12 @@ type cacheEntry struct {
 	createdAt time.Time
 }
 
-func NewCache() Cache {
-	return Cache{
+func NewCache(interval time.Duration) Cache {
+	c := Cache{
 		cache: make(map[string]cacheEntry),
 	}
+	go c.reapLoop(interval)
+	return c
 }
 
 func (c *Cache) Add(key string, val []byte) {
@@ -30,4 +32,20 @@ func (c *Cache) Get(key string) ([]byte, bool) {
 		return nil, false
 	}
 	return cacheE.val, true
+}
+
+func (c *Cache) reapLoop(interval time.Duration) {
+	ticker := time.NewTicker(interval)
+	for range ticker.C {
+		c.reap(interval)
+	}
+}
+
+func (c *Cache) reap(interval time.Duration) {
+	timeAgo := time.Now().UTC().Add(-interval)
+	for k, v := range c.cache {
+		if v.createdAt.Before(timeAgo) {
+			delete(c.cache, k)
+		}
+	}
 }
